3dd4c5dc68e474e970ad7eb034b516923d1d76d8
david espinal <despinal0425@gmail.com>
2020-11-02 12:56:40 -0500

    added method to add, delete, toggle true or false to todo list

--- js/index.js
+++ js/index.js
@@ -1,3 +1,73 @@
+//Grouping properties and methods in relation to the todoList
 let todoList = {
-  todos: ['item 1', 'item 2', 'item 3'],
+  todos: [],
+  displayTodos: function () {
+    //if there are no todos, then log 'Your todo list is empty!'
+    //else print todo as normal
+    if (this.todos.length === 0) { 
+      console.log('Your todo list is empty!');
+    } else {
+      console.log('My Todos:');
+      //looks at todoText property of each object in the array
+      for (let i = 0; i < this.todos.length; i++) { 
+        //check if .completed is true, then add (x)
+        //else add ( )
+        if (this.todos[i].completed === true) {
+          console.log('(x)', this.todos[i].todoText);
+        } else { 
+          console.log('( )', this.todos[i].todoText);
+        }
+      }
+    }
+  },
+  addTodo: function (todoText) { 
+    //pushing object to todos array
+    this.todos.push({
+      todoText: todoText,
+      completed: false
+    });
+    this.displayTodos();
+  },
+  changeTodo: function (position, todoText) { 
+    // Changes object's todoText property's value
+    this.todos[position].todoText = todoText;
+    this.displayTodos()
+  },
+  deleteTodo: function (position) { 
+    this.todos.splice(position, 1);
+    this.displayTodos();
+  },
+  toggleCompleted: function (position) { 
+    let todo = this.todos[position];
+    //flip boolean from false to true or true to false in object
+    todo.completed = !todo.completed;
+    this.displayTodos();
+  },
+  toggleAll: function () { 
+    let totalTodos = this.todos.length;
+    let completedTodos = 0;
+
+    //get number of completed todos
+    for (let i = 0; i < totalTodos; i++) { 
+      if (this.todos[i].completed === true) { 
+        completedTodos++;
+      }
+    }
+
+    //if eveything is true, then make everything is false
+    //if everything is fase, then make everything is true
+    if (completedTodos === totalTodos) {
+      //make everything false
+      for (let i = 0; i < totalTodos; i++) {
+        this.todos[i].completed = false;
+      }
+    } else { 
+      for (let i = 0; i < totalTodos; i++) { 
+        this.todos[i].completed = true;
+      }
+    }
+    this.displayTodos();
+  }
 };
+
+
